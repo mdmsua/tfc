50e0189fd6d673f0343c8318782fc522789e1b7f
Dmytro Morozov <mdmsua@icloud.com>
2025-12-26 13:19:17 +0100

    chore: Add github provider variables

--- registry/providers.tf
+++ registry/providers.tf
@@ -30,4 +30,11 @@ provider "azurerm" {
 
 provider "azapi" {}
 
-provider "github" {}
+provider "github" {
+  owner = var.github_owner
+  app_auth {
+    id              = var.github_app_id
+    installation_id = var.github_app_installation_id
+    pem_file        = var.github_app_pem_file
+  }
+}
--- registry/variables.tf
+++ registry/variables.tf
@@ -15,3 +15,24 @@ variable "docker_hub_token" {
   type        = string
   sensitive   = true
 }
+
+variable "github_app_id" {
+  description = "GitHub app ID"
+  type        = string
+}
+
+variable "github_app_installation_id" {
+  description = "GitHub app installation ID"
+  type        = string
+}
+
+variable "github_app_pem_file" {
+  description = "GitHub app PEM file"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_owner" {
+  description = "GitHub owner"
+  type        = string
+}
