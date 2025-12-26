efb8942123e5bd08f11a1d9718ada58177ebfc53
Dmytro Morozov <mdmsua@icloud.com>
2025-12-26 11:17:45 +0100

    chore: Add ModSecurity image

--- registry/main.tf
+++ registry/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_container_registry" "main" {
   sku                    = "Basic"
 }
 
-resource "azurerm_container_registry_task" "main" {
+resource "azurerm_container_registry_task" "agent" {
   name                  = "tfc-agent"
   container_registry_id = azurerm_container_registry.main.id
 
@@ -22,7 +22,7 @@ resource "azurerm_container_registry_task" "main" {
   }
 
   docker_step {
-    dockerfile_path      = "image/Dockerfile"
+    dockerfile_path      = "images/agent/Dockerfile"
     context_path         = "https://github.com/mdmsua/tfc"
     context_access_token = var.github_token
     image_names          = ["tfc-agent:latest"]
@@ -34,6 +34,36 @@ resource "azurerm_container_registry_task" "main" {
   }
 }
 
+resource "azurerm_container_registry_task" "modsecurity" {
+  name                  = "modsecurity"
+  container_registry_id = azurerm_container_registry.main.id
+
+  platform {
+    os           = "Linux"
+    architecture = "arm64"
+  }
+
+  docker_step {
+    dockerfile_path      = "images/modsecurity/Dockerfile"
+    context_path         = "https://github.com/mdmsua/tfc"
+    context_access_token = var.github_token
+    image_names          = ["modsecurity:${var.modsecurity_version}"]
+
+    arguments = {
+      VERSION = var.modsecurity_version
+    }
+  }
+
+  timer_trigger {
+    name     = "daily"
+    schedule = "0 0 * * *"
+  }
+}
+
+moved {
+  from = azurerm_container_registry_task.main
+  to   = azurerm_container_registry_task.agent
+}
 resource "azapi_update_resource" "registry_role_assignment_mode" {
   type        = "Microsoft.ContainerRegistry/registries@2025-04-01"
   resource_id = azurerm_container_registry.main.id
@@ -42,4 +72,20 @@ resource "azapi_update_resource" "registry_role_assignment_mode" {
       roleAssignmentMode = "AbacRepositoryPermissions"
     }
   }
+}
+
+resource "azurerm_role_assignment" "container_registry_repository_catalog_lister" {
+  for_each = var.contributors
+
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "Container Registry Repository Catalog Lister"
+  principal_id         = each.key
+}
+
+resource "azurerm_role_assignment" "container_registry_repository_contributor" {
+  for_each = var.contributors
+
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "Container Registry Repository Contributor"
+  principal_id         = each.key
 }
--- registry/variables.tf
+++ registry/variables.tf
@@ -3,3 +3,15 @@ variable "github_token" {
   type        = string
   sensitive   = true
 }
+
+variable "contributors" {
+  description = "Registry contributors"
+  type        = set(string)
+  default     = ["6b1aa092-b266-49f3-be05-341fff39cd59"]
+}
+
+variable "modsecurity_version" {
+  description = "ModSecurity version"
+  type        = string
+  default     = "4.21.0"
+}
