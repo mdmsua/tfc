6fe9afa1b40477e11d67364a3182d635ee74948f
Dmytro Morozov <mdmsua@icloud.com>
2026-01-04 16:56:48 +0100

    chore(cluster): Disable AzureAD in provider config

--- cluster/providers.tf
+++ cluster/providers.tf
@@ -63,6 +63,7 @@ provider "azurerm" {
   resource_provider_registrations = "none"
   client_id_file_path             = var.tfc_azure_dynamic_credentials.default.client_id_file_path
   oidc_token_file_path            = var.tfc_azure_dynamic_credentials.default.oidc_token_file_path
+  storage_use_azuread             = false
 }
 
 provider "tfe" {
